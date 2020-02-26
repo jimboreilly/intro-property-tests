6d82db5790ff40d3e802c517a7cbf89c26f1b1d7
Jim Reilly <jimreilly28@gmail.com>
2020-02-26 15:17:02 -0600

    test that sorted list is a permutation of the original

--- Sorting.Spec/SortingSpec.cs
+++ Sorting.Spec/SortingSpec.cs
@@ -1,25 +1,51 @@
 using NUnit.Framework;
-using Sorting;
-using System;
 using System.Linq;
 using FsCheck;
-
+using PropertyAttribute = FsCheck.NUnit.PropertyAttribute;
+using System.Collections.Generic;
 
 namespace Sorting.Spec
 {
+    [TestFixture]
     public class SortingSpec
     {
 
-        [Test]
-        public void SortedListIsOrderedAscending()
+        [Property]
+        public static bool SortedListIsAscendingPairwise(int[] array)
         {
-            Func<int[], bool> leftNeverGreaterThanRight = array =>
-            {
-                var sorted = array.SlowSort().ToList();
-                return sorted.Zip(sorted.Skip(1)).All(pair => pair.First <= pair.Second);
-            };
-
-            Prop.ForAll(leftNeverGreaterThanRight).QuickCheckThrowOnFailure();
+            var sorted = array.SlowSort().ToList();
+            return sorted.Zip(sorted.Skip(1)).All(pair => pair.First <= pair.Second);
         }
+
+        [Property]
+        public static bool SortedListMaintainsSizeOfOriginal(int[] array) => array.SlowSort().Count() == array.Count();
+
+        [Property]
+        public static bool SortedListIsPermutationOfOriginal(int[] array)
+        {
+            var originalMap = new Dictionary<int, int>();
+            foreach (var x in array)
+            {
+                if (originalMap.ContainsKey(x)) originalMap[x] += 1;
+                else originalMap.Add(x, 1);
+            }
+
+            var sorted = array.SlowSort();
+
+            foreach (var x in sorted)
+            {
+                if (!originalMap.ContainsKey(x) || originalMap[x] == 0) return false;
+                if (originalMap.ContainsKey(x)) originalMap[x] -= 1;
+            }
+
+            foreach (var pair in originalMap)
+            {
+                if (pair.Value != 0) return false;
+            }
+
+            return true;
+        }
+
+
     }
 }
