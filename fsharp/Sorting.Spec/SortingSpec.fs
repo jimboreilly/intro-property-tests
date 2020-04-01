aaf0d628bf8f0d5d7890755bb1a35a45cd5b808c
JamesMReilly <jimreilly28@gmail.com>
2020-04-01 18:07:12 -0500

    a property test of a list in fsharp

---
+++ fsharp/Sorting.Spec/SortingSpec.fs
@@ -0,0 +1,17 @@
+﻿module SortingSpec
+
+open NUnit.Framework
+open FsCheck
+
+[<Test>]
+let ``a sorted list is same size as original``() =
+    let sizeIsMaintained (x: int list) =
+        let left =
+            x
+            |> Seq.sort
+            |> Seq.length
+
+        let right = x |> Seq.length
+        left = right
+
+    Check.QuickThrowOnFailure sizeIsMaintained
