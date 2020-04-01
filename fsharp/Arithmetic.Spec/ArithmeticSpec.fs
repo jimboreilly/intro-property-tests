2c9dd8edc9e43f8edb68010f3dc47dc87194d002
JamesMReilly <jimreilly28@gmail.com>
2020-04-01 17:17:01 -0500

    test addition

--- fsharp/Arithmetic.Spec/ArithmeticSpec.fs
+++ fsharp/Arithmetic.Spec/ArithmeticSpec.fs
@@ -2,12 +2,25 @@
 
 open NUnit.Framework
 open FsUnit
+open FsCheck
 
 open Example.Arithmetic
 
 
 [<Test>]
-let ``this is a test test``() = add 2 2 |> should equal 4
+let ``addition is associative``() =
+    let associativeProperty a b c = add a (add b c) = add (add a b) c
+
+    Check.QuickThrowOnFailure associativeProperty
 
 [<Test>]
-let ``this one is also a test``() = multiply 2 3 |> should equal 6
+let ``addition is commutative``() =
+    let commutativeProperty a b = add a b = add b a
+
+    Check.QuickThrowOnFailure commutativeProperty
+
+[<Test>]
+let ``the additive identity is zero``() =
+    let identity a = add a 0 = a
+
+    Check.QuickThrowOnFailure identity
