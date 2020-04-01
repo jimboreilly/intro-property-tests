2d81f24480ee0a4d5bd2d3052f99f4202bde884e
JamesMReilly <jimreilly28@gmail.com>
2020-04-01 17:03:29 -0500

    add an fsharp solution

---
+++ fsharp/Arithmetic.Spec/ArithmeticSpec.fs
@@ -0,0 +1,13 @@
+﻿module ArithmeticSpec
+
+open NUnit.Framework
+open FsUnit
+
+open Example.Arithmetic
+
+
+[<Test>]
+let ``this is a test test``() = add 2 2 |> should equal 4
+
+[<Test>]
+let ``this one is also a test``() = multiply 2 3 |> should equal 6
---
+++ fsharp/Arithmetic/Arithmetic.fs
@@ -0,0 +1,6 @@
+﻿namespace Example
+
+module Arithmetic =
+    let add x y = x + y
+
+    let multiply x y = x * y
