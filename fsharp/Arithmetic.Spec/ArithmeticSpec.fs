dd5893d22e93432548be5c1d56b5b5fa29bfeaef
JamesMReilly <jimreilly28@gmail.com>
2020-04-01 17:18:18 -0500

    add multiplication tests

--- fsharp/Arithmetic.Spec/ArithmeticSpec.fs
+++ fsharp/Arithmetic.Spec/ArithmeticSpec.fs
@@ -20,7 +20,25 @@ let ``addition is commutative``() =
     Check.QuickThrowOnFailure commutativeProperty
 
 [<Test>]
-let ``the additive identity is zero``() =
+let ``the additive identity is 0``() =
     let identity a = add a 0 = a
+
+    Check.QuickThrowOnFailure identity
+
+[<Test>]
+let ``multiplication is associative``() =
+    let associativeProperty a b c = multiply a (multiply b c) = multiply (multiply a b) c
+
+    Check.QuickThrowOnFailure associativeProperty
+
+[<Test>]
+let ``multiplication is commutative``() =
+    let commutativeProperty a b = multiply a b = multiply b a
+
+    Check.QuickThrowOnFailure commutativeProperty
+
+[<Test>]
+let ``the multiplicative identity is 1``() =
+    let identity a = multiply a 1 = a
 
     Check.QuickThrowOnFailure identity
