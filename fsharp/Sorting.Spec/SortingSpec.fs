8682e0d1f0f722babbea2eee81bcf405f5726ca7
JamesMReilly <jimreilly28@gmail.com>
2020-04-01 20:12:24 -0500

    check if sorted list is permutation of original

--- fsharp/Sorting.Spec/SortingSpec.fs
+++ fsharp/Sorting.Spec/SortingSpec.fs
@@ -25,3 +25,20 @@ let ``a sorted list is ordered ascending pairs``() =
         |> Seq.forall (fun (a, b) -> a <= b)
 
     Check.QuickThrowOnFailure orderedPairs
+
+[<Test>]
+let ``sorted list is a permutation of the original list``() =
+    let rec removeElem elem list =
+        match list with
+        | [] -> raise (System.ArgumentException("value is not in the list to be removed"))
+        | car :: cdr when car = elem -> cdr
+        | car :: cdr -> car :: (removeElem elem cdr)
+
+    let rec isPermutation aList bList =
+        match aList with
+        | [] -> List.isEmpty bList
+        | car :: cdr -> List.contains car bList && isPermutation cdr (removeElem car bList)
+
+    let sortedIsPermutationOfOriginal (aList: int list) = isPermutation aList (List.sort aList)
+
+    Check.QuickThrowOnFailure sortedIsPermutationOfOriginal
